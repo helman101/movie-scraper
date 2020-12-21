f4e13c1591fdf4456f016ec530c0cfbabbd303a1
helman101 <helmanandresortegon@gmail.com>
2020-12-21 16:04:05 +0100

    add search_movies method to Search class

--- lib/search.rb
+++ lib/search.rb
@@ -10,4 +10,15 @@ class Search
     @doc = Nokogiri::HTML(URI.open("https://123moviesfree.net/search-query/#{movie_url}"))
     @movie = Movie.new(@doc)
   end
+
+  def search_movies
+    @movie.movies if @movie.results == true
+    if @movie.results == true
+      puts '
+Select your movie with a number
+Or use /n (Next) or /b (Back) commands to change pages
+And use /s (Stop) to exit'
+    end
+    command(gets.chomp.strip) if @movie.results == true
+  end
 end
